811f1a19c977feb47eff9c5424f28ca84b788c18
lnnjdcbrr04 <lnnjdcbrr.04@gmail.com>
2026-02-10 19:59:06 +0800

    Feat: Adding file for multiplication and addition

---
+++ Ariola_Github_Calculator_Sample/Addition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ariola_Github_Calculator_Sample
+{
+    internal class Addition
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+            //This is addition operator
+        }
+
+    }
+}
---
+++ Ariola_Github_Calculator_Sample/Multiplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ariola_Github_Calculator_Sample
+{
+    internal class Multiplication
+    {
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+            //This is a Multiplication operator
+        }
+    }
+}
