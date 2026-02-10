817bfcd6149548b323644e4517bef1736b10edc4
AriolaTiffany <ariolatiffy@gmail.com>
2026-02-10 19:57:11 +0800

    feat: adding 2 new files for subtraction and division operators

---
+++ Ariola_Github_Calculator_Sample/division.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ariola_Github_Calculator_Sample
+{
+    internal class division
+    {
+
+        public double Division(double num1, double num2)
+        {
+            return num1 / num2;
+            // this is division operator
+        }
+    }
+}
+
